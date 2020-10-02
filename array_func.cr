1c32b57be0020d07fb7a078c705825d25f4dd193
lamarmarshall75 <lamarmarshall75@outlook.com>
2020-10-02 01:59:44 -0400

    array_func

---
+++ array_func.cr
@@ -0,0 +1,14 @@
+arr = [ 25, 35, 67, 44, 21, 10]
+
+p arr.map{|x| x * 17}
+
+p arr.any?{|x| x < 25} # should be true
+p arr.any?{ |x| x > 100 } # should be false
+
+arr.each_with_index do | x, i|
+	p "#{x} for #{i}"
+end
+
+p arr.includes?(5)
+
+p arr.find{ |i| i > 60}
